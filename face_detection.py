c5ed6ddfda87824d91cb30e053a22b7289ffa9bf
ZIPPY\David <ravy101@gmail.com>
2019-12-05 22:07:20 +1100

    fixed face detection and improved prediction system

--- face_detection.py
+++ face_detection.py
@@ -1,41 +1,53 @@
 import cv2
+import numpy as np
 
+pad_factor = .25
+out_folder = 'video_faces/'
 face_path = "haarcascade_frontalface_default.xml"
 
 face_cascade = cv2.CascadeClassifier(face_path)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
-video_capture = cv2.VideoCapture(0)
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
+cap = cv2.VideoCapture('8.mov')
+flip = True
 
-    grayscale_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+i = 0
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if flip:
+        frame = cv2.flip(frame, -1)
+    grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
 
     faces = face_cascade.detectMultiScale(
-        grayscale_image,
+        grey_frame,
         scaleFactor=1.1,
         minNeighbors=5,
-        minSize=(200, 200),
+        minSize=(100, 100),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
 
+
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 3)
-        roi_gray = grayscale_image[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-        cv2.putText(frame,'Face',(x, y), font, 2,(255,0,0),5)
+        padx = int(pad_factor * w)
+        pady = int(pad_factor * h)
+        cv2.rectangle(grey_frame, (x-padx, y-pady), (x+w+padx, y+h+pady), (255, 0, 0), 3)
+        face_colour = frame[y-pady:y+h+pady, x-padx:x+w+padx]
+        cv2.putText(grey_frame,'Face',(x, y), font, 2,(255,0,0),5)
+        cv2.imwrite(out_folder + str(i) + ".jpg",face_colour)
+        i = i + 1
+
+
 
 
     cv2.putText(frame,'Number of Faces : ' + str(len(faces)),(40, 40), font, 1,(255,0,0),2)      
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+    cv2.imshow('Video', grey_frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
       break
 
-# When everything is done, release the capture
-video_capture.release()
+cap.release()
 cv2.destroyAllWindows()
--- use_existing.py
+++ use_existing.py
@@ -6,7 +6,7 @@ from tensorflow import keras
 from keras.preprocessing.image import array_to_img, img_to_array, load_img, ImageDataGenerator
 
 model_name = 'stride_model.m5'
-img_folder = 'test_images/'
+img_folder = 'video_faces/'
 
 #Read command line arguments
 if(len(sys.argv) > 2):
@@ -22,9 +22,15 @@ existing_model = keras.models.load_model(model_name)
 
 #load and prepare test files
 img_files = os.listdir(img_folder)
-test_images = np.array([img_to_array(load_img(img_folder + file, target_size=(128,128))) for file in img_files])
+images = []
+for file in img_files:
+    try:
+        images.append(img_to_array(load_img(img_folder + file, target_size=(128,128))))
+    except:
+        pass
+test_images = np.array(images)
 test_images = test_images * 1./255
 
 #make predicitons and output
 predictions = existing_model.predict_classes(test_images)
-print(predictions)
+print(predictions == 1)
